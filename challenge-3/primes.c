1b7aa627b75fc0e23502347060737dd5b873dd16
elliot <elliotbielwasem@gmail.com>
2018-08-20 21:36:48 -0400

    added descriptions of challenges to files

--- challenge-3/primes.c
+++ challenge-3/primes.c
@@ -1,3 +1,8 @@
+/**
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ */
+
 // Program to print all prime factors
 # include <stdio.h>
 # include <math.h>
