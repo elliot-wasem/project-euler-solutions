8e04a2dc017e5e658e5c1e24c2f1ed649296c4fe
elliot <elliotbielwasem@gmail.com>
2018-08-20 22:08:30 -0400

    completed challenge 5

--- challenge-5/src/main.rs
+++ challenge-5/src/main.rs
@@ -1,3 +1,22 @@
+/*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+
 fn main() {
-    println!("Hello, world!");
+    let mut number: u64 = 20;
+    loop {
+        println!("\t\tnumber: {:?}", number);
+        for i in 11..21 {
+            println!("\t\t\ti: {:?}", i);
+            if number % i != 0 {
+                break;
+            }
+            if i == 20 {
+                println!("result: {:?}", number);
+                return ();
+            }
+        }
+        number += 20;
+    }
 }
